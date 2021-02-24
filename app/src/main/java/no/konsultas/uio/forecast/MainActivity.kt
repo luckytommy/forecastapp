5a36d2b2815d92a0b8eba9a53511cb879d8ab1b3
luckytommy <thomas.lindsjorn@gmail.com>
2021-02-24 19:01:21 +0100

    Made a simple UI, populated with dummy data for now

--- app/src/main/java/no/konsultas/uio/forecast/MainActivity.kt
+++ app/src/main/java/no/konsultas/uio/forecast/MainActivity.kt
@@ -2,10 +2,25 @@ package no.konsultas.uio.forecast
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<View>(R.id.search_button).setOnClickListener {
+            val searchQuery = findViewById<EditText>(R.id.search_bar).text.toString()
+            putDummyDataInResultView(searchQuery)
+        }
+    }
+
+    private fun putDummyDataInResultView(searchQuery: String) {
+        findViewById<TextView>(R.id.search_result_city_name).text = searchQuery
+        findViewById<TextView>(R.id.search_result_degrees).text = "25"
+        findViewById<TextView>(R.id.search_result_description).text = "Sunny"
     }
 }
